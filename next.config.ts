10487eac8eed9ef8f22d704f631fcc8e1645234f
Gurudatt Choudhary <119776344+gurudattch@users.noreply.github.com>
2026-06-08 11:20:30 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,35 @@
 import type { NextConfig } from "next";
 
+
+
 const nextConfig: NextConfig = {
+
+
   reactStrictMode: true,
 
+
+
+
   images: {
+
     unoptimized: true,
+
   },
+
+
+  reactStrictMode: true,
+
+
+  typescript: {
+
+
+    ignoreBuildErrors: true,
+
+
+  },
+
 };
+
+
 
 export default nextConfig;
